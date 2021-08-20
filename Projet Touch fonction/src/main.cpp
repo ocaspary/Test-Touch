8e805d2ae0d80f37e2d11392f024555a648aeb1b
Olivier Caspary <ocaspary@free.fr>
2021-08-20 10:56:48 +0200

    Projet Touch fonction

--- Projet Touch fonction/src/main.cpp
+++ Projet Touch fonction/src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// set pin numbers
+const int touchPin = 4; // GPIO4
+const int ledPin = 2;   // GPIO2 : Blue LED of the NodeMCU
+
+// change with your threshold value
+const int threshold = 25;
+// variable for storing the touch pin value 
+int touchValue;
+
+void touchDetect(int ledPin, int touchPin, int threshold){
+  // read the state of the pushbutton value:
+  touchValue = touchRead(touchPin);
+  Serial.print(touchValue);
+  // check if the touchValue is below the threshold
+  // if it is, set ledPin to HIGH
+  if(touchValue < threshold){
+    // turn LED on
+    digitalWrite(ledPin, HIGH);
+    Serial.println(" - LED on");
+  }
+  else{
+    // turn LED off
+    digitalWrite(ledPin, LOW);
+    Serial.println(" - LED off");
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup(){
+  Serial.begin(115200);
+  delay(1000); // give me time to bring up serial monitor
+  // initialize the LED pin as an output:
+  pinMode (ledPin, OUTPUT);
 }
+
+void loop(){
+  touchDetect(2, 4, 25);
+  delay(500);
+}
+
